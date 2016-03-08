04d762434fbb3b2e541d82c173e5cb3efd1b463a
Allen Mills <Allen_Mills@mail.fresnostate.edu>
2016-03-08 14:23:44 -0800

    [Task1] Back at it. Working on functionality. Local version looks good. Will start using appengine for datastore soon.

--- Task1/main.go
+++ Task1/main.go
@@ -1,11 +1,12 @@
 package main
 
 import (
+	"fmt"
 	"html/template"
 	"net/http"
 
 	"github.com/julienschmidt/httprouter"
-	"google.golang.org/appengine/datastore"
+	//"google.golang.org/appengine/datastore"
 )
 
 var pages *template.Template
@@ -17,10 +18,10 @@ func init() {
 	r.GET("/showMessage", showMsg)
 	r.GET("/makeMessage", makeMsg)
 	r.POST("/makeMessage", uploadMsg)
-	r.get("/favicon.ico", favIcon)
+	r.GET("/favicon.ico", favIcon)
 	http.Handle("/public/", http.StripPrefix("/public", http.FileServer(http.Dir("public/"))))
 
-	pages = template.Must(tpl.ParseGlob("html/*.html"))
+	pages = template.Must(pages.ParseGlob("html/*.html"))
 }
 
 func home(res http.ResponseWriter, req *http.Request, params httprouter.Params) {
@@ -32,18 +33,40 @@ func home(res http.ResponseWriter, req *http.Request, params httprouter.Params) 
 
 func showMsg(res http.ResponseWriter, req *http.Request, params httprouter.Params) {
 	// Retrive form data from datastore
+
 	// post message onto form webpage.
+	err := pages.ExecuteTemplate(res, "showMessage.html", "Test Message")
+	if err != nil {
+		http.Error(res, err.Error(), http.StatusInternalServerError)
+	}
+
 }
 
 func makeMsg(res http.ResponseWriter, req *http.Request, params httprouter.Params) {
 	// Post the form to edit data
+	err := pages.ExecuteTemplate(res, "makeMessage.html", nil)
+	if err != nil {
+		http.Error(res, err.Error(), http.StatusInternalServerError)
+	}
 }
 
 func uploadMsg(res http.ResponseWriter, req *http.Request, params httprouter.Params) {
+
+	// ctx := appengine.NewContext(req)
+	// key := datastore.NewKey(ctx, "Users", req.FormValue("Message"), 0, nil)
+
+	fmt.Println("Data Taken from submit", req.FormValue("Message"))
 	// upload form data to datastore
+
 	// redirect to home
+	err := pages.ExecuteTemplate(res, "showMessage.html", req.FormValue("Message"))
+	if err != nil {
+		http.Error(res, err.Error(), http.StatusInternalServerError)
+	}
+	//http.Redirect(res, req, "/", http.StatusTemporaryRedirect)
+
 }
 
 func favIcon(res http.ResponseWriter, req *http.Request, params httprouter.Params) {
-	http.Redirect(res, req, "public/images/favicon.ico", 302)
+	http.Redirect(res, req, "public/images/favicon.ico", http.StatusTemporaryRedirect)
 }
