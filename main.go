2ac60c529250afd5e76ef6f56d13e6c7ae90b0f6
Allen Mills <Mills.Allen.James@gmail.com>
2016-05-02 12:31:45 -0700

    [URLS] Changed all deleters to post calls. Refer to the wiki for information regarding post parameters.

--- main.go
+++ main.go
@@ -33,7 +33,7 @@ func init() {
 	r.GET("/image/uploader", IMAGE_PostUploadForm)                      // image uploader <user-internal><auth>
 	r.GET("/api/getImage", IMAGE_API_GetImageFromCS)                    // image requester <user-internal>
 	r.POST("/api/makeImage", IMAGE_API_PlaceImageIntoCS)                // image creator <api><auth>
-	r.GET("/api/deleteImage", IMAGE_API_RemoveImageFromCS)              // image deleter <api><auth>
+	r.POST("/api/deleteImage", IMAGE_API_RemoveImageFromCS)             // image deleter <api><auth>
 	r.POST("/api/ckeditor/create", IMAGE_API_CKEDITOR_PlaceImageIntoCS) // ckEditor, image creator <api><auth>
 
 	// API.go, readers - Collection
@@ -58,12 +58,11 @@ func init() {
 	r.POST("/api/makeObjective", API_MakeObjective) // create datastore, objective <api><auth>
 
 	// API.go, deleters
-	// TODO: change to post
-	r.GET("/api/deleteCatalog", API_DeleteCatalog)     // delete datastore, catalog <api><auth>
-	r.GET("/api/deleteBook", API_DeleteBook)           // delete datastore, book <api><auth>
-	r.GET("/api/deleteChapter", API_DeleteChapter)     // delete datastore, chapter <api><auth>
-	r.GET("/api/deleteSection", API_DeleteSection)     // delete datastore, section <api><auth>
-	r.GET("/api/deleteObjective", API_DeleteObjective) // delete datastore, objective <api><auth>
+	r.POST("/api/deleteCatalog", API_DeleteCatalog)     // delete datastore, catalog <api><auth>
+	r.POST("/api/deleteBook", API_DeleteBook)           // delete datastore, book <api><auth>
+	r.POST("/api/deleteChapter", API_DeleteChapter)     // delete datastore, chapter <api><auth>
+	r.POST("/api/deleteSection", API_DeleteSection)     // delete datastore, section <api><auth>
+	r.POST("/api/deleteObjective", API_DeleteObjective) // delete datastore, objective <api><auth>
 
 	// main.go
 	r.GET("/", home)                         // Root page <user>
