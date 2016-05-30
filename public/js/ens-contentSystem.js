78576d9519d53adefbc9aff7311e378ac006bb94
John Redden <jtredden@gmail.com>
2016-05-29 17:37:08 -0700

    CKeditor config now in global js file.... refactoring
    clean-up redundand code.

--- public/js/ens-contentSystem.js
+++ public/js/ens-contentSystem.js
@@ -2,5 +2,41 @@
        return window.setTimeout( callback, seconds * 1000 );
     }
 
+// no images ckeditor config
+var noImagesConfig = {
+  	extraPlugins: 'mathjax',
+  	mathJaxLib: 'http://cdn.mathjax.org/mathjax/latest/MathJax.js?config=TeX-MML-AM_CHTML',
+   	removePlugins: 'forms'
+ };
 
+ // images ckeditor requires an editity id
+ function stdImageConfig(argID){
+    var stdConfig = {
+            extraPlugins: 'mathjax',
+            mathJaxLib: 'http://cdn.mathjax.org/mathjax/latest/MathJax.js?config=TeX-MML-AM_CHTML',
+            filebrowserImageBrowseUrl: '/image/browser?action=browseImageURL&oid='+argID,
+            filebrowserImageUploadUrl: '/api/ckeditor/create?action=uploadImageURL&oid='+argID,
+            filebrowserBrowseUrl: '/image/browser?action=browseURL&oid='+argID,
+            filebrowserUploadUrl: '/api/ckeditor/create?action=uploadURL&oid='+argID,
+            removePlugins: 'forms',
+    };
+    return stdConfig;
+ }
+
+// code stolen to overried the save event
+CKEDITOR.plugins.registered['save'] = {
+  init: function (editor) {
+     var command = editor.addCommand('save',
+     {
+          modes: { wysiwyg: 1, source: 1 },
+          exec: function (editor) { // Add here custom function for the save button
+            //alert('You clicked the save button in CKEditor toolbar!');
+            $('.saveBtn').trigger('click');
+          }
+     });
+     editor.ui.addButton('Save', { label: 'Save', command: 'save' });
+  }
+}
+
+CKEDITOR.config.contentsCss = '/public/css/myeditor.css';
 
